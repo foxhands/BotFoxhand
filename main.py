3db286f91ddb5447e559bd5ec97dfcda23c2448f
Ruslan <foxhand7@gmail.com>
2024-02-28 10:08:59 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,70 @@
-# This example requires the 'message_content' privileged intents
-
 import os
-import discord
-from discord.ext import commands
 
+import discord
+
+import requests
 
 intents = discord.Intents.default()
 intents.message_content = True
-bot = commands.Bot(command_prefix='!', intents=intents)
+
+client = discord.Client(intents=intents)
 
 
-@bot.event
+@client.event
 async def on_ready():
-    print(f"Logged in as {bot.user}")
-
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
-
-@bot.command()
-async def hello(ctx):
-    await ctx.send("Choo choo! 🚅")
+  print('We have logged in as {0.user}'.format(client))
 
 
-bot.run(os.environ["DISCORD_TOKEN"])
+@client.event
+async def on_message(message):
+  if message.author == client.user:
+    return
+
+  # Handle link messages in a separate function
+  await handle_links(message)
+
+
+async def handle_links(message):
+  if message.content.startswith((
+      'https://www.youtube.com/watch?v=',
+      'https://youtu.be/',
+      'https://www.youtube.com/shorts/',
+      'https://www.youtube.com/playlist?list=',
+      'https://vm.tiktok.com/',
+      'https://www.tiktok.com/',
+      'https://www.tiktok.com/@',
+      'https://www.tiktok.com/t/',
+      'https://www.tiktok.com/video/',
+  )):
+    # Search for the thread (case-sensitive)
+    thread = discord.utils.get(message.guild.threads, name='Видосики')
+
+    if thread is None:
+      # Inform user if the thread is not found
+      await message.channel.send(f"The {thread} thread was not found.")
+
+    else:
+      # Send a message to the thread
+      await thread.send(f"@everyone {message.author}: {message.content}")
+
+    await message.delete()
+
+  # Search for the thread (case-sensitive)
+  #thread = discord.utils.get(message.guild.threads, name='Картинки')
+
+
+try:
+  token = os.getenv("TOKEN") or ""
+  if token == "":
+    raise Exception("Please add your token to the Secrets pane.")
+  client.run(token)
+except discord.HTTPException as e:
+  if e.status == 429:
+    print(
+        "The Discord servers denied the connection for making too many requests"
+    )
+    print(
+        "Get help from https://stackoverflow.com/questions/66724687/in-discord-py-how-to-solve-the-error-for-toomanyrequests"
+    )
+  else:
+    raise e

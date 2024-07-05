32d2f5c34e950374c3f4eacc935e5beee1c2f63f
Ruslan <foxhand7@gmail.com>
2024-07-06 01:53:45 +0300

    Update main.py

--- main.py
+++ main.py
@@ -105,7 +105,7 @@ async def handle_files(message):
                 await handle_image(attachment, image_thread, message)
             elif any(attachment.filename.lower().endswith(ext) for ext in ('.mp4', '.mov', '.avi', '.mkv')):
                 await handle_video(attachment, video_thread, message)
-            elif any(attachment.filename.lower().endswith(ext) for ext in ('.zip', '.rar')):
+            elif any(attachment.filename.lower().endswith(ext) for ext in ('.zip', '.rar', '.pdf', '.docx')):
                 await handle_zip(attachment, file_thread, message)
 
 
